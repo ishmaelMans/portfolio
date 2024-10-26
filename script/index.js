ed8a9e97ae862e3e4e4f6da27e4d24a98485acce
ishmaelMans <mansarayishmael17@gmail.com>
2024-10-25 22:11:53 -0600

    animation

--- script/index.js
+++ script/index.js
@@ -10,79 +10,77 @@ let lastScrollY = window.scrollY;
 let per;
 // navbar list..........................................................
 const checkForHightLight = (e) => {
-	removeHighLight();
-	e.target.classList.add("current");
+  removeHighLight();
+  e.target.classList.add("current");
 };
 
 const removeHighLight = () => {
-	checkList.forEach((list) => list.classList.remove("current"));
+  checkList.forEach((list) => list.classList.remove("current"));
 };
 checkList.forEach((list) => list.addEventListener("click", checkForHightLight));
 
 // Hamburger button......................................................
 function toggle() {
-	btnClose.classList.toggle("active");
-	menuBtn.classList.toggle("open");
-	btnOpen.classList.toggle("active");
+  btnClose.classList.toggle("active");
+  menuBtn.classList.toggle("open");
+  btnOpen.classList.toggle("active");
 }
 btnOpen.addEventListener("click", toggle, false);
 
 // Remove side nav........................................................
 list.forEach((each) => {
-	each.addEventListener("click", () => {
-		btnClose.classList.toggle("active");
-		menuBtn.classList.toggle("open");
-	});
+  each.addEventListener("click", () => {
+    btnClose.classList.toggle("active");
+    menuBtn.classList.toggle("open");
+  });
 });
 
 //Nav Animation...........................................................
 const navAnimation = () => {
-	if (lastScrollY < window.scrollY) {
-		navBar.classList.add("active");
-	} else {
-		navBar.classList.remove("active");
-	}
-	lastScrollY = window.scrollY;
+  if (lastScrollY < window.scrollY) {
+    navBar.classList.add("active");
+  } else {
+    navBar.classList.remove("active");
+  }
+  lastScrollY = window.scrollY;
 };
 
 // Progress Bar Starts.....................................................
 scrollElement.forEach((el) => {
-	per = el.getAttribute("per");
-	el.style.width = per;
+  per = el.getAttribute("per");
+  el.style.width = per;
 });
 
 const elementInView = (el) => {
-	const elementTop = el.getBoundingClientRect().top;
-	return (
-		elementTop <=
-		(window.innerHeight || document.documentElement.clientHeight)
-	);
+  const elementTop = el.getBoundingClientRect().top;
+  return (
+    elementTop <= (window.innerHeight || document.documentElement.clientHeight)
+  );
 };
 const elementOutofView = (el) => {
-	const elementTop = el.getBoundingClientRect().top;
-	return (
-		elementTop >
-		(window.innerHeight || document.documentElement.clientHeight)
-	);
+  const elementTop = el.getBoundingClientRect().top;
+  return (
+    elementTop > (window.innerHeight || document.documentElement.clientHeight)
+  );
 };
 const displayScrollElement = (element) => {
-	element.classList.add("progress");
+  element.classList.add("progress");
 };
 const hideScrollElement = (element) => {
-	element.classList.remove("progress");
+  element.classList.remove("progress");
 };
 const handleScrollAnimation = () => {
-	scrollElement.forEach((el) => {
-		if (elementInView(el)) {
-			displayScrollElement(el);
-		} else if (elementOutofView(el)) {
-			hideScrollElement(el);
-		}
-	});
+  scrollElement.forEach((el) => {
+    if (elementInView(el)) {
+      displayScrollElement(el);
+    } else if (elementOutofView(el)) {
+      hideScrollElement(el);
+    }
+  });
 };
 window.addEventListener("scroll", () => {
-	handleScrollAnimation();
-	navAnimation();
+  handleScrollAnimation();
+  navAnimation();
 });
 
 // carousel .......................................................
@@ -91,15 +89,31 @@ const review = document.querySelectorAll(".container");
 let index = 0;
 
 function run() {
-	index++;
-	if (index > review.length - 1) {
-		index = 0;
-	}
-	reviews.style.transform = `translateX(${-index * 410}px )`;
+  index++;
+  if (index > review.length - 1) {
+    index = 0;
+  }
+  reviews.style.transform = `translateX(${-index * 410}px )`;
 }
 
 setInterval(() => {
-	run();
+  run();
 }, 5000);
 
 //....................................................................
+
+// video iteration
+let video = document.getElementById("myVideo");
+video.control = false;
+
+function playVideo() {
+  video.currentTime = 0;
+  video.play();
+}
+
+setTimeout(playVideo, 5000);
+
+video.addEventListener("ended", function () {
+  video.pause();
+  setTimeout(playVideo, 5000);
+});
